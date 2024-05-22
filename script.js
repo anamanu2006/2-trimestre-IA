f2ff469f9b96b41a68cd672216ded7c5f0058a9b
anamanu2006 <ana.goltz@escola.pr.gov.br>
2024-05-22 08:52:40 -0300

    22 de maio

---
+++ script.js
@@ -0,0 +1,21 @@
+const caixaPrincipal = documento.quarySelector(".caixa-principal");
+const caixaPerguntas = documento.quarySelector(".caixa-perguntas");
+const caixaAlternativas = documento.quarySelector(".caixa-alternativas");
+const caixaResultado = documento.quarySelector(".caixa-resultado");
+const textoResultado = documento.quarySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+}
+];
